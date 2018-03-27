f689a971d8809e9ffee4ef4b6cd270ca0d25538f
Yashar <ya.atajan@gmail.com>
2018-03-27 16:43:53 -0400

    main page UI done

--- kids joy center/Main/MainVC.swift
+++ kids joy center/Main/MainVC.swift
@@ -24,19 +24,64 @@ class MainVC: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func memoryBtnPushed(_ sender: Any) {
+    @IBAction func memoryBtnPushed(_ sender: UIButton) {
+        gameChoiceAction(sender)
     }
-    @IBAction func sortingBtnPushed(_ sender: Any) {
+    @IBAction func sortingBtnPushed(_ sender: UIButton) {
+        gameChoiceAction(sender)
     }
-    @IBAction func balloonBtnPushed(_ sender: Any) {
+    @IBAction func balloonBtnPushed(_ sender: UIButton) {
+        gameChoiceAction(sender)
     }
-    @IBAction func easyBtnPushed(_ sender: Any) {
+    @IBAction func easyBtnPushed(_ sender: UIButton) {
+        levelChoiceAction(sender)
     }
-    @IBAction func mediumBtnPushed(_ sender: Any) {
+    @IBAction func mediumBtnPushed(_ sender: UIButton) {
+        levelChoiceAction(sender)
     }
-    @IBAction func hardBtnPushed(_ sender: Any) {
+    @IBAction func hardBtnPushed(_ sender: UIButton) {
+        levelChoiceAction(sender)
     }
-    @IBAction func playBtnPushed(_ sender: Any) {
+    @IBAction func playBtnPushed(_ sender: UIButton) {
     }
+    
+    func gameChoiceAction(_ button: UIButton) {
+        hideGameChosenOptions()
+        showChosenOptions(button)
+        enableLevelOptions()
+    }
+    
+    func levelChoiceAction(_ button: UIButton) {
+        hideLevelChosenOption()
+        showChosenOptions(button)
+        enablePlayButton()
+    }
+    
+    func enableLevelOptions() {
+        easyBtn.isEnabled = true
+        mediumBtn.isEnabled = true
+        hardBtn.isEnabled = true
+    }
+    
+    func enablePlayButton() {
+        playBtn.isEnabled = true
+    }
+    
+    func showChosenOptions(_ button: UIButton) {
+        button.alpha = 1.0
+        enableLevelOptions()
+    }
+    
+    func hideGameChosenOptions() {
+        memoryBtn.alpha = 0.5
+        sortingBtn.alpha = 0.5
+        balloonBtn.alpha = 0.5
+    }
+    func hideLevelChosenOption() {
+        easyBtn.alpha = 0.5
+        mediumBtn.alpha = 0.5
+        hardBtn.alpha = 0.5
+    }
+    
 }
 
